39d4e0fc0e7d342de2f54d500839a101135339cd
szidieross <szidieross@gmail.com>
2023-10-08 11:04:17 +0300

    2.3

--- hf2/index.php
+++ hf2/index.php
@@ -10,6 +10,10 @@
         .piros {
             color: red;
         }
+
+        .bold {
+            font-weight: bold;
+        }
     </style>
 </head>
 
@@ -54,9 +58,29 @@
         echo "<p>$orszag fővárosa <span class='piros'>$fovaros</span></p>";
     }
 
+
     echo "<h2>3. feladat</h2>";
 
-    
+    $napok = array(
+        "HU" => array("H", "K", "Sze", "Cs", "P", "Szo", "V"),
+        "EN" => array("M", "Tu", "W", "Th", "F", "Sa", "Su"),
+        "DE" => array("Mo", "Di", "Mi", "Do", "F", "Sa", "So"),
+    );
+
+    foreach ($napok as $nyelv => $het) {
+        $sor = "<span>$nyelv:</span>";
+        for ($i = 0; $i < count($het); $i++) {
+            if ($i == count($het) - 1) {
+                $sor .= "<span> $het[$i]</span>";
+            } elseif ($i % 2 == 1) {
+                $sor .= "<span class='bold'> $het[$i]</span>";
+            } else {
+                $sor .= "<span> $het[$i],</span>";
+            }
+        }
+        echo "$sor<br>";
+    }
+
     echo "<h2>4. feladat</h2>";
     echo "<h2>5. feladat</h2>";
     ?>
