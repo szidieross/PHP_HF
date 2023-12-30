1d1dc02bd7ac3206b567811c5a6739abd766c188
szidieross <szidieross@gmail.com>
2023-12-30 15:39:11 +0200

    file upload

--- hf6/form1.php
+++ hf6/form1.php
@@ -7,26 +7,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
     $email = isset($_POST["email"]) ? $_POST["email"] : '';
     $attend = isset($_POST["attend"]) ? $_POST["attend"] : [];
     $tshirt = isset($_POST["tshirt"]) ? $_POST["tshirt"] : '';
-    // $abstract = $_POST["abstract"];
-    $abstractFileName = isset($_FILES['abstract']) ? $_FILES['abstract'] : '';
-
-    // $abstract=$_FILES["abstract"];
+    $abstractFile = isset($_FILES['abstract']) ? $_FILES['abstract'] : '';
     $terms = isset($_POST["terms"]);
-
-    echo "$firstName\n";
-    echo "$tshirt\n";
-    var_dump($attend);
-    // var_dump($abstract);
-    var_dump($abstractFileName);
-    echo "\n" . $terms;
 
     if (isset($_FILES['abstract']) && $_FILES['abstract']['error'] === UPLOAD_ERR_OK) {
         $abstractFile = $_FILES['abstract'];
 
-        var_dump($abstractFile);
     } else {
         echo "Error uploading file.";
     }
+    
+    echo "$firstName\n";
+    echo "$tshirt\n";
+    var_dump($attend);
+    var_dump($abstractFile);
+    echo "\n" . $terms;
 }
 echo "</pre>";
 ?>
